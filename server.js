c83ff9b96075045a5d37580a3f5cb0c63041dfd0
harkaran Singh <harkaran3114@gmail.com>
2024-10-21 11:43:13 +0530

    this

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(core());
+
+
+//APP CONFIG START
+app.listen(port,()=>{
+    console.log(`server running on port https://localhost:${port}`);
+})
